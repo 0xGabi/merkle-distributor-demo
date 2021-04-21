5ed12f57d6de8dd206dda75fbb4c64929f6c5b6c
Gabriel <0xgabi@gmail.com>
2021-04-21 18:54:52 -0300

    🧹

---
+++ scripts/demo.ts
@@ -0,0 +1,12 @@
+import hre from 'hardhat'
+
+async function main() {
+  hre.ethers
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
